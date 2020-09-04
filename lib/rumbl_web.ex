2d9a88e8ca3a18a1d48afb0cc51c284e3aa45d39
seiichi <poipoipoi33378@gmail.com>
2020-09-04 16:36:00 +0900

    add video

--- lib/rumbl_web.ex
+++ lib/rumbl_web.ex
@@ -23,6 +23,7 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
+      import RumblWeb.Auth, only: [authenticate_user: 2]
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
@@ -45,9 +46,9 @@ defmodule RumblWeb do
   def router do
     quote do
       use Phoenix.Router
-
       import Plug.Conn
       import Phoenix.Controller
+      import RumblWeb.Auth, only: [authenticate_user: 2]
     end
   end
 
