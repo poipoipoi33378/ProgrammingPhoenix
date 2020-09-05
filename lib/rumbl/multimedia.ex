bdae2808f86a6be65e14ce5b3d7439577c74cc89
seiichi <poipoipoi33378@gmail.com>
2020-09-05 15:05:07 +0900

    modify video index

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -49,9 +49,11 @@ defmodule Rumbl.Multimedia do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_video(attrs \\ %{}) do
+  alias Rumbl.Accounts
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
   end
 
