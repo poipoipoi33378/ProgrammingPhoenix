97c5f639884c3698115935faa7ce21a778428c39
seiichi <poipoipoi33378@gmail.com>
2020-09-07 16:22:14 +0900

    add accounts_test

--- test/rumbl/accounts_test.exs
+++ test/rumbl/accounts_test.exs
@@ -18,5 +18,34 @@ defmodule Rumbl.AccountsTest do
       assert user.username == "eva"
       assert [%User{id: ^id}] = Accounts.list_users()
     end
+
+    test "with invalid data does not insert user" do
+      assert {:error, _changeset} = Accounts.register_user(@invalid_attrs)
+      assert Accounts.list_users() == []
+    end
+
+    test "enforces unique usernames" do
+      assert {:ok, %User{id: id}} = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset} = Accounts.register_user(@valid_attrs)
+
+      assert %{username: ["has already been taken"] } = errors_on(changeset)
+      assert [%User{id: ^id}] = Accounts.list_users()
+    end
+
+    test "does not accept long usernames" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a",30))
+      assert {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{username: ["should be at most 20 character(s)"] } = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+
+    test "requires password to be at least 6 chars long" do
+      attrs = Map.put(@valid_attrs, :password, "12345")
+      assert {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{username: ["should be at least 6 character(s)"] } = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
   end
 end
